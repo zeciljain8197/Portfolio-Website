3197cdbb3f2bfd46085d4abdf2d76d33a7000f80
Zecil Jain <zecil.jain@gmail.com>
2022-11-13 00:12:01 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=350)
+    st.image("images/photo.png", width=400)
 
 with col2:
     st.title("Zecil Jain")
