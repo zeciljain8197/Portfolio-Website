2189fa788c1368acd90a7b139f19d5c0987b0133
Zecil Jain <zecil.jain@gmail.com>
2022-11-21 11:27:37 +0530

    Updated Home.py

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png", width=400)
+    st.image("images/photo.png", width=500)
 
 with col2:
     st.title("Zecil Jain")
@@ -26,14 +26,14 @@ col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pd.read_csv("data.csv", sep=";")
 
 with col3:
-    for index, row in df[:5].iterrows():
+    for index, row in df[:7].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
 
 with col4:
-    for index, row in df[5:].iterrows():
+    for index, row in df[7:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
