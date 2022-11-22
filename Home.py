26d0b4d2905c4180372ebe792d0ab5985a82629e
Zecil Jain <zecil.jain@gmail.com>
2022-11-22 22:08:55 +0530

    Updated Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,21 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(page_title="Zecil Jain Portfolio Website", page_icon="🖖")
 st.set_page_config(layout='wide')
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
+
+padding = 0
+st.markdown(f""" <style>
+    .reportview-container .main .block-container{{
+        padding-top: {padding}rem;
+        padding-right: {padding}rem;
+        padding-left: {padding}rem;
+        padding-bottom: {padding}rem;
+    }} </style> """, unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
 
